e304d6276cb9a497f90289613bf91e8293f0815f
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2025-11-08 22:18:02 +0530

    Update CORS origin to allow all origins

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin : "http://localhost:5173",
+    origin : true,
     credentials : true
 }
 app.use(cors(corsOption));
