c61d992a4efa54a6f0edc41804ce0cc6730bda6e
krishnam2003 <singhkrishnam2003@gmail.com>
2025-11-09 11:00:08 +0530

    last update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin : true,            //"http://localhost:5173"
+    origin : "*",
     credentials : true
 }
 app.use(cors(corsOption));
